531fef4b21012e241e5414df8f0f9a6a7224136c
Lucero Terrazas Cendejas <luzterra17@hotmail.com>
2020-06-30 15:25:07 -0500

    Initial Commit

---
+++ Tablas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tablas
+//
+//  Created by Lucero Terrazas Cendejas on 6/30/20.
+//  Copyright © 2020 Erika Lucero Terrazas Cendejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
